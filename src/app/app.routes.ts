39516918f569d54dae365e2a09e68402bd670870
CDalianis <christos_dl@hotmail.com>
2026-05-06 12:08:18 +0300

    feat add: user login on routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,7 +9,10 @@ import { Step9ComponentOutput } from './components/step9-component-output/step9-
 import { Step10TemplateDrivenForms } from './components/step10-template-driven-forms/step10-template-driven-forms';
 import { Step11ReactiveForms } from './components/step11-reactive-forms/step11-reactive-forms';
 import { Step12HttpClientService } from './components/step12-http-client-service/step12-http-client-service';
-
+import { Step13UserLogin } from './components/step13-user-login/step13-user-login';
+import { RestrictedContent } from './components/restricted-content/restricted-content';
+import { authGuard } from './shared/guards/auth-guard';
+import { readerRoleGuard } from './shared/guards/reader-role-guard';
 
 export const routes: Routes = [
   {path:'person-table-example', component:Step2PersonTable},
@@ -21,6 +24,12 @@ export const routes: Routes = [
   {path:'template-driven-form-example', component:Step10TemplateDrivenForms},
   {path:'reactive-form-example', component:Step11ReactiveForms},
   {path:'http-client-example', component:Step12HttpClientService},
+  {path:'user-login', component: Step13UserLogin},
+  {
+    path: 'restricted-content',
+    component: RestrictedContent,
+    canActivate: [authGuard, readerRoleGuard]
+  },
   {path:'welcome', component:Step6Welcome},
   {path:'', redirectTo:'/welcome', pathMatch:'full'}
 ];
